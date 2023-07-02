e353f29af748285b98fda85e01fcb5f49d3ccc84
PramodhTVK <pramodhkannan1829@gmail.com>
2023-07-02 19:24:07 +0530

    new project using three js

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import { headContainerAnimation, headContentAnimation, headTextAnimation }
+
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
