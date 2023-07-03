6e23024f3c8d012a32f4c50e7a30398d98cc1bbb
PramodhTVK <pramodhkannan1829@gmail.com>
2023-07-04 00:00:22 +0530

    Environment and camera

---
+++ client/src/canvas/CameraRig.jsx
@@ -0,0 +1,21 @@
+import React,{ useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { easing } from 'maath'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
+
+const CameraRig = ({ children }) => {
+    const group = useRef()
+    const snap = useSnapshot(state)
+
+    
+    //set the model rotation smoothly
+    easing.dampE(group.current.rotation,[state.pointer.y /10,state.pointer.x /5,0],0.25)
+
+  return (
+    <group>{children}</group>
+  )
+}
+
+export default CameraRig
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,26 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment,Center } from '@react-three/drei'
+import Shirt from './Shirt'
+import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
 
 const CanvasModel = () => {
   return (
-    <div>CanvasModel</div>
+    <Canvas>   {/* Canvas is a component from @react-three/fiber.It is a wrapper provided by react-three-fiber that sets up a WebGL Canvas ans creates a 3D Rendering Context */}
+      {/*Web GL context is an object that represents the interface between JS code and WebGL APIs.It provides a way to interact with the WebGL API  */}
+      <ambientLight intensity={0.5} />{/* ambientLight is a component from @react-three/fiber that adds ambient light to a scene.In React Three Fiber, the ambientLight component is a light source that provides ambient lighting to a 3D scene. Ambient lighting is a type of indirect lighting that evenly illuminates all objects in the scene, simulating the light that is scattered and reflected from various surfaces. */}
+      <Environment preset="city" />{/* Environment is a component from @react-three/drei that provides a 360 degree background.
+      In React Three Fiber, the <Environment> component is used to add a pre-built environment to a 3D scene. It provides a quick and convenient way to set up a realistic background and lighting for your scene.
+      The preset prop in the <Environment> component allows you to choose from a variety of pre-defined environment presets that are included with the library. These presets are pre-configured 3D environments that provide a realistic backdrop, lighting, and reflections for your scene. */}
+      <CameraRig>
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    
+    
+    </Canvas>
   )
 }
 
