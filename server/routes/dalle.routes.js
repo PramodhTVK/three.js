0312838d04ef379b985aa9c880fbf126d9cfa848
PramodhTVK <pramodhkannan1829@gmail.com>
2023-08-07 22:39:03 +0530

    route

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -51,16 +51,20 @@ router.route('/').post(async (req, res) => {
     const response = await openai.createImage({
       prompt,
       n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json'
+      size: "1024x1024",
+      response_format: "b64_json"
     });
 
     const image = response.data.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Something went wrong" })
+    if (error.response) {
+      console.error("OpenAI API Error:", error.response.data.error);
+    } else {
+      console.error("Error:", error.message);
+    }
+    res.status(500).json({ message: "Something went wrong" });
   }
 })
 
