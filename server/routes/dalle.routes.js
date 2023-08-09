ac75d3c2c2141da8e14788b84d45066dcdcff409
PramodhTVK <pramodhkannan1829@gmail.com>
2023-08-09 05:37:55 +0530

    route testing

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -34,11 +34,11 @@ dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const config = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(config);
+// const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
@@ -46,18 +46,28 @@ router.route('/').get((req, res) => {
 
 router.route('/').post(async (req, res) => {
   try {
-    const { prompt } = req.body;
+    // const { prompt } = req.body;
 
-    const response = await openai.createImage({
-      prompt,
-      n: 1,
-      size: "1024x1024",
-      response_format: "b64_json"
-    });
+    // const response = await openai.createImage({
+    //   prompt,
+    //   n: 1,
+    //   size: "1024x1024",
+    //   response_format: "b64_json"
+    // });
 
-    const image = response.data.data[0].b64_json;
+    // const image = response.data.data[0].b64_json;
 
-    res.status(200).json({ photo: image });
+    // res.status(200).json({ photo: image });
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createImage({
+  prompt: "A cute baby sea otter",
+  n: 2,
+  size: "1024x1024",
+});
+
   } catch (error) {
     if (error.response) {
       console.error("OpenAI API Error:", error.response.data.error);
